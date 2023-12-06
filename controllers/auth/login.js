ba5c598f4cb2e032cc7b1095738a4d6c82e45b25
Марк Неловка <87484591+Mark-Nelovka@users.noreply.github.com>
2023-12-06 23:51:02 +0200

    Change time for jwt in login path

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -20,7 +20,7 @@ const login = async (req, res) => {
   const accessToken = jwt.sign(
     { uid: user._id, sid: newSession._id },
     JWT_ACCESS_SECRET_KEY,
-    { expiresIn: '1h' },
+    { expiresIn: '1m' },
   );
   const refreshToken = jwt.sign(
     { uid: user._id, sid: newSession._id },
