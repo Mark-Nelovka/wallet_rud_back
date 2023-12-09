529d308ef97f6547fb276fd0dd50b0b1de798ec4
Марк Неловка <87484591+Mark-Nelovka@users.noreply.github.com>
2023-12-09 12:58:16 +0200

    Change format for result set Transactions

--- controllers/transactions/setTransaction.js
+++ controllers/transactions/setTransaction.js
@@ -2,11 +2,14 @@ const {Transaction} = require("../../models")
 
 const setTransaction = async (req, res) => {
   const {_id: owner} = req.user
-  console.log(_id);
+  console.log("req.user._id: ", req.user._id);
+  console.log("req.body: ", req.body)
   try {
     const result = await Transaction.create({...req.body, owner})
-    console.log(result);
-    res.status(201).json(`info: ${JSON.stringify(result)}`);
+    console.log("result: ", result);
+    res.status(201).json({
+      info: result
+    });
   } catch (error) {
     return error;
   }
