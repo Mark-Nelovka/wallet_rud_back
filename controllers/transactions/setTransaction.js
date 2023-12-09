c5fdb43798b7213b55f5368ad8e31678c4566595
Марк Неловка <87484591+Mark-Nelovka@users.noreply.github.com>
2023-12-09 16:51:46 +0200

    Change response for set transactions

--- controllers/transactions/setTransaction.js
+++ controllers/transactions/setTransaction.js
@@ -2,13 +2,26 @@ const {Transaction} = require("../../models")
 
 const setTransaction = async (req, res) => {
   const {_id: owner} = req.user
-  console.log("req.user._id: ", req.user._id);
+  console.log("req.user: ", req.user);
   console.log("req.body: ", req.body)
   try {
     const result = await Transaction.create({...req.body, owner})
-    console.log("result: ", result);
+    const transactios = {
+      _id: result._id,
+      date: result.date,
+      type: result.type,
+      category: result.category,
+      comment: result.comment || "",
+      sum: result.sum,
+      balance: result.balance,
+      owner: {
+        _id: req.user._id,
+        name: req.user.name,
+        email: req.user.email
+      }
+    }
     res.status(201).json({
-      info: result
+      info: transactios
     });
   } catch (error) {
     return error;
@@ -17,3 +30,23 @@ const setTransaction = async (req, res) => {
 }
 
 module.exports = setTransaction;
+// {
+//   "email": "test_email_test@ukr.net",
+//   "password": "password1Q"
+// }
+// {
+//   "date": "Sat Dec 09 2023 16:24:25",
+// "type": "income",
+// "category": "Products",
+// "comment": "safdsafsf",
+// "sum": 2000,
+// "balance": 2000
+
+// }
+    //   {
+    //   info: {...result, _id: req.user._id, owner: {
+    //     _id: req.user._id,
+    //     name: req.user.name,
+    //     email: req.user.email
+    //   }}
+    // }
